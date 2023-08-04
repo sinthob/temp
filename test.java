47ffb35908cd66659cf39e3aa31e40e7d8e4fc25
Sinthob Katasila <sinthob.k@kkumail.com>
2023-08-04 14:36:35 +0700

    first commit

---
+++ test.java
@@ -0,0 +1,10 @@
+public class test {
+    String name = "Sinthob";
+    String lastname = "Katasila";
+    static String fullname = "Sinthob Katasila";
+
+    public static void main(String[] args) {
+        System.out.println(fullname);
+    }
+
+}
